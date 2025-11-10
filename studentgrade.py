9ee715553f2e2d722ed9ccb5ee62ad0d52a718c8
Shrigouri-h <shrigouri0707@gmail.com>
2025-11-10 14:43:47 +0530

    save

--- studentgrade.py
+++ studentgrade.py
@@ -1,9 +1,25 @@
-maths=99
-english=85  
-science=78
-history=88  
-hindi=99
-average = maths+english+science+history+hindi/ 5
+import sys
+
+# Check if user provided 5 subject marks
+# Expected input: python grade_calc.py m1 m2 m3 m4 m5
+if len(sys.argv) == 6:
+    script_name = sys.argv[0]
+    m1 = float(sys.argv[1])
+    m2 = float(sys.argv[2])
+    m3 = float(sys.argv[3])
+    m4 = float(sys.argv[4])
+    m5 = float(sys.argv[5])
+    print("User provided marks:")
+else:
+    script_name = sys.argv[0]
+    # Default marks
+    m1, m2, m3, m4, m5 = 80, 75, 90, 85, 70
+    print("No input given - using default marks:")
+
+# Calculate average
+average = (m1 + m2 + m3 + m4 + m5) / 5
+
+# Determine grade
 if average >= 90:
     grade = 'A'
 elif average >= 75:
@@ -14,5 +30,8 @@ elif average >= 40:
     grade = 'D'
 else:
     grade = 'Fail'
+
+# Display results
+print("Script Name:", script_name)
 print("Average Marks:", average)
 print("Grade:", grade)
